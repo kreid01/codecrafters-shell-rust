af5b19093434a48ac2620a3f135615e66ab25d54
Kieran Reid <kieranreid2612@gmail.com>
2025-11-10 21:23:44 +0000

    implemented type for echoing builtin shell command

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,26 @@ fn main() -> ExitCode {
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
 
-        if command.contains("exit") {
+        if command.starts_with("exit") {
             return ExitCode::from(0);
-        } else if command.contains("echo") {
+        } else if command.starts_with("echo") {
             print!("{}", str::replace(&command, "echo ", ""))
+        } else if command.starts_with("type") {
+            get_type(command);
         } else {
-            println!("{}: command not found", command.trim());
         }
     }
+}
+
+fn get_type(command: String) {
+    let t = command.replacen("type ", "", 1);
+    if t.starts_with("type") || t.starts_with("echo") || t.starts_with("exit") {
+        println!("{} is a shell builtin", t.trim());
+    } else {
+        not_found(command)
+    }
+}
+
+fn not_found(command: String) {
+    println!("{}: command not found", command.trim());
 }
