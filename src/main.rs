d5ba7a92d6d99be961419a43b5133ee7714be3ba
Kieran Reid <kieranreid2612@gmail.com>
2025-11-12 21:00:02 +0000

    from example cause im stuck

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
-use std::fs;
 use std::io::{self, Write};
-use std::path::Path;
+use std::os::unix::fs::PermissionsExt;
 use std::process::ExitCode;
 
 fn main() -> ExitCode {
@@ -29,39 +28,27 @@ fn get_type(command: String) {
     if t.starts_with("type") || t.starts_with("echo") || t.starts_with("exit") {
         println!("{} is a shell builtin", t.trim());
     } else {
-        check_dirs(command)
+        check_dirs(&command)
     }
 }
 
-fn check_dirs(command: String) {
-    let binding = env::var("PATH").unwrap();
-    let dirs: Vec<&str> = binding.split(":").collect();
+fn check_dirs(command: &String) {
+    if let Ok(paths) = env::var("PATH") {
+        let t = command.replacen("type ", "", 1);
 
-    let t = command.replacen("type ", "", 1);
-    let mut found = false;
-
-    for dir in dirs {
-        let candidate = Path::new(dir).join(&t);
-
-        if candidate.is_file() {
-            {
-                use std::os::unix::fs::PermissionsExt;
-                let meta = candidate.metadata().unwrap();
-                if meta.permissions().mode() & 0o111 != 0 {
-                    println!("{} is {}", t, candidate.display());
-                    found = true;
-                    break;
+        for dir in env::split_paths(&paths) {
+            let file = dir.join(command);
+            if file.is_file() {
+                if let Ok(meta) = file.metadata() {
+                    println!("{}", file.to_string_lossy());
+                    if meta.permissions().mode() & 0o111 != 0 && file.to_string_lossy() == t {
+                        println!("{} is {}", t.trim(), dir.display());
+                    }
                 }
             }
         }
     }
-
-    if !found {
-        command_not_found(command)
-    }
 }
-
-// fn check_dirs(dir: &str) {}
 
 fn not_found(command: String) {
     println!("{}: command not found", command.trim());
