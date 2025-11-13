dd2532029b8ba82f9aa80adf7c92e4ab490978a8
Kieran Reid <kieranreid2612@gmail.com>
2025-11-13 21:47:02 +0000

    codecrafters submit [skip ci]

---
+++ src/change_directory.rs
@@ -0,0 +1,80 @@
+use std::env;
+use std::path::{Path, PathBuf};
+
+pub fn cd(command: &str) {
+    let directory = command.split_whitespace().nth(1).unwrap();
+
+    if directory.starts_with("./") {
+        cd_relative(command);
+        return;
+    } else if directory.starts_with("../") {
+        cd_back(command);
+        return;
+    } else if directory.starts_with("~") {
+        cd_home();
+        return;
+    }
+
+    if cd_absolute(directory).is_none() {
+        no_file_or_directory(directory);
+    }
+}
+
+pub fn cd_absolute(directory: &str) -> Option<()> {
+    let path = Path::new(directory);
+    let new_dir_path = path.join(directory);
+    return go_to_directory(new_dir_path);
+}
+
+pub fn cd_home() -> Option<()> {
+    if let Ok(home) = env::var("HOME") {
+        let path = Path::new(&home);
+        return go_to_directory(path.to_path_buf());
+    }
+
+    None
+}
+
+pub fn cd_back(command: &str) -> Option<()> {
+    let curr_dir = get_curr_directory();
+    let back_count = command.split("./").count();
+    let path = Path::new(&curr_dir)
+        .ancestors()
+        .take(back_count)
+        .last()
+        .unwrap();
+
+    return go_to_directory(path.to_path_buf());
+}
+
+pub fn get_curr_directory() -> PathBuf {
+    let curr_dir = env::current_dir().unwrap();
+    return Path::new(&curr_dir).to_path_buf();
+}
+
+pub fn cd_relative(directory: &str) -> Option<()> {
+    let curr_dir = get_curr_directory();
+    let next_dir = directory
+        .split_whitespace()
+        .nth(1)
+        .unwrap()
+        .replace("./", "");
+    let next_dir_path = curr_dir.join(next_dir);
+
+    return go_to_directory(next_dir_path);
+}
+
+pub fn go_to_directory(directory: PathBuf) -> Option<()> {
+    if directory.is_dir() {
+        return Some(assert!(env::set_current_dir(&directory).is_ok()));
+    }
+
+    None
+}
+
+pub fn no_file_or_directory(directory: &str) {
+    println!(
+        "cd: {}: No such file or directory",
+        directory.split_whitespace().nth(0).unwrap()
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use std::env;
 use std::io::{self, Write};
 use std::os::unix::fs::PermissionsExt;
-use std::path::Path;
 use std::process::Command;
 use std::process::ExitCode;
-use std::{env, path};
+
+mod change_directory;
 
 const BUILTINS: [&str; 5] = ["exit", "echo", "type", "pwd", "cd"];
 
@@ -23,7 +24,7 @@ fn main() -> ExitCode {
             "echo" => print!("{}", str::replace(&command, "echo ", "")),
             "type" => execute_type(&command),
             "pwd" => pwd(),
-            "cd" => cd(&command),
+            "cd" => change_directory::cd(&command),
             _ => execute(&command),
         };
     }
@@ -46,72 +47,6 @@ pub fn execute(command: &str) {
         let output = String::from_utf8_lossy(&input.stdout);
         print!("{}", output);
     }
-}
-
-fn cd(command: &str) {
-    let next_dir = command.split_whitespace().nth(1).unwrap();
-
-    if next_dir.starts_with("./") {
-        cd_relative(command);
-        return;
-    } else if next_dir.starts_with("../") {
-        cd_back(command);
-        return;
-    }
-
-    let path = path::Path::new(next_dir);
-    let new_dir_path = path.join(next_dir);
-    if new_dir_path.is_dir() {
-        assert!(env::set_current_dir(&new_dir_path).is_ok());
-        return;
-    }
-
-    println!("cd: {}: No such file or directory", next_dir)
-}
-
-fn cd_back(command: &str) {
-    let curr_dir = env::current_dir().unwrap();
-    let count = command.split("./").count();
-    let path = path::Path::new(&curr_dir)
-        .ancestors()
-        .take(count)
-        .last()
-        .unwrap();
-
-    if path.is_dir() {
-        assert!(env::set_current_dir(&path).is_ok());
-        return;
-    }
-
-    return;
-}
-
-fn cd_relative(command: &str) {
-    let curr_dir = env::current_dir().unwrap();
-    let path = path::Path::new(&curr_dir);
-    let next_dir = command.split_whitespace().nth(1).unwrap().replace("./", "");
-    let new_dir_path = path.join(next_dir);
-
-    if new_dir_path.is_dir() {
-        assert!(env::set_current_dir(&new_dir_path).is_ok());
-        return;
-    }
-
-    // for dirs in path {
-    //     let replaced_dir = &mut next_dir.replace("./", "");
-    //     println!("{} and new {}", , replaced_dir.to_string());
-    //     if dirs.to_string_lossy() == replaced_dir.to_string() {
-    //         if new_dir_path.is_dir() {
-    //             assert!(env::set_current_dir(&new_dir_path).is_ok());
-    //             return;
-    //         }
-    //     }
-    // }
-
-    println!(
-        "cd: {}: No such file or directory",
-        command.split_whitespace().nth(0).unwrap()
-    )
 }
 
 fn pwd() {
