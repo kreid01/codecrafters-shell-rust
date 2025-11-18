3b6f0aff789d179f6921f0e16d355d21892c4ed4
Kieran Reid <kieranreid2612@gmail.com>
2025-11-18 20:43:46 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use std::env;
+use std::env::{self};
+use std::fs;
 use std::io::{self, Write};
 use std::os::unix::fs::PermissionsExt;
+use std::path::{self};
 use std::process::Command;
 use std::process::ExitCode;
 
 mod change_directory;
 mod parser;
+mod writer;
 
 const BUILTINS: [&str; 5] = ["exit", "echo", "type", "pwd", "cd"];
 
@@ -27,30 +30,92 @@ fn main() -> ExitCode {
             "pwd" => pwd(),
             "cd" => change_directory::cd(&command),
             "cat" => cat(&command),
+            "ls" => ls(command),
             _ => execute(&command),
         };
     }
 }
 
+pub fn ls(command: String) {
+    let command_split: Vec<&str> = command.split_whitespace().collect();
+    if command_split.len() > 3 {
+        if command_split[3] == ">" || command_split[3] == "1>" {
+            let input_path = command_split[2];
+            let output_path = path::Path::new(command_split[4]);
+            let entries = fs::read_dir(input_path).expect("Cannot read directory");
+
+            let mut lines = Vec::new();
+            for entry in entries {
+                let entry = entry.unwrap();
+                let file_name = entry.file_name().to_string_lossy().to_string();
+                lines.push(file_name);
+            }
+
+            lines.sort();
+            let _ = writer::write(output_path.to_path_buf(), lines);
+        }
+    } else {
+        let current_dir = change_directory::get_curr_directory();
+        for x in current_dir.iter() {
+            println!("{}", x.display());
+        }
+    }
+}
+
 pub fn cat(command: &str) {
     let command_wo_cat = str::replace(&command, "cat ", "");
-    let args = parser::get_formatted_args(&command_wo_cat);
+    let command_split: Vec<&str> = command_wo_cat.split("1>").collect();
 
-    let input = match Command::new("cat").args(&args.clone()).output() {
+    if command_split.len() > 1 {
+        let args = parser::get_formatted_args(&command_split[0]);
+        let output_path = path::Path::new(command_split[1].trim());
+
+        for path in args {
+            if !path::Path::new(&path).is_file() {
+                println!("cat: {}: No such file or directory", path);
+            } else if let Some(call) = Some(call_cat(command.to_string(), vec![path])) {
+                let _ = writer::write(output_path.to_path_buf(), vec![call.unwrap()]);
+            }
+        }
+    } else {
+        let args = parser::get_formatted_args(&command);
+        if let Some(call) = Some(call_cat(command.to_string(), args)) {
+            println!("{}", call.unwrap());
+        }
+    }
+}
+
+pub fn call_cat(path: String, args: Vec<String>) -> Option<String> {
+    let input = match Command::new("cat").args(&args).output() {
         Ok(output) => output,
         Err(_) => {
-            println!("{}: command not found", command.trim());
-            return;
+            println!("{}: Command not found", path.trim());
+            return None;
         }
     };
 
-    println!("{}", String::from_utf8_lossy(&input.stdout).trim_end());
+    return Some(
+        String::from_utf8_lossy(&input.stdout)
+            .trim_end()
+            .to_string(),
+    );
 }
 
 pub fn echo(command: String) {
     let command_wo_echo = str::replace(&command, "echo ", "");
+    let command_split: Vec<&str> = command_wo_echo.split("1>").collect();
     let formatted_command = parser::format_string_command(&command_wo_echo);
-    println!("{}", &formatted_command.trim())
+
+    if command_split.len() > 1 {
+        let mut contents = Vec::new();
+        let input_path = command_split[0];
+        let formatted_command = parser::format_string_command(&input_path).trim().to_owned();
+        let output_path = path::Path::new(command_split[1].trim());
+        contents.push(formatted_command);
+        let _ = writer::write(output_path.to_path_buf(), contents);
+    } else {
+        println!("{}", &formatted_command.trim())
+    }
 }
 
 pub fn execute(command: &str) {
@@ -88,7 +153,6 @@ fn is_command_in_path(command: &str) -> Option<String> {
     if let Ok(paths) = env::var("PATH") {
         for dir in env::split_paths(&paths) {
             let path = dir.join(command);
-
             if path.is_file() {
                 if let Ok(metadata) = path.metadata() {
                     let permissions = metadata.permissions();
---
+++ src/writer.rs
@@ -0,0 +1,15 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{self, LineWriter};
+use std::path::PathBuf;
+
+pub fn write(file_name: PathBuf, contents: Vec<String>) -> io::Result<()> {
+    let file = File::create(file_name)?;
+    let mut file = LineWriter::new(file);
+
+    for x in contents {
+        let buff = format!("{}\n", x);
+        file.write_all(buff.as_bytes())?;
+    }
+    Ok(())
+}
