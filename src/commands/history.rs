851c7ba2adc2bf515d276806fc8cab6fb977ae74
Kieran Reid <kieranreid2612@gmail.com>
2025-11-28 20:17:34 +0000

    codecrafters submit [skip ci]

--- src/commands/history.rs
+++ src/commands/history.rs
@@ -1,7 +1,8 @@
 pub fn history(cmd: &str, history: &[String]) {
     let count = cmd.replace("history ", "").parse::<usize>().unwrap_or(10);
+    let diff = history.len() - count;
 
-    for (i, command) in history.iter().enumerate().take(count) {
+    for (i, command) in history.iter().enumerate().skip(diff) {
         println!("\t{} {}", i + 1, command)
     }
 }
