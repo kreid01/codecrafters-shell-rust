a7b50358cb49745da79a58ae960749fca9ef24ad
Kieran Reid <kieranreid2612@gmail.com>
2025-11-29 11:17:57 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -60,6 +60,7 @@ fn main() -> ExitCode {
                     .filter(|x| !appended_history.contains(x))
                     .map(|x| x.to_string())
                     .collect();
+                println!("{}", history_to_append);
                 history::append_file_history(cmd, &history_to_append);
                 appended_history = history.to_owned();
                 break;
